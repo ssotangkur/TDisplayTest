d9d419b252ac84e7ce58c893b2423dc7dbca8b9c
Sarit Sotangkur <ssotangkur@yahoo.com>
2025-12-22 21:56:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Test Project using the LilyGO T-Display
+
+## Reference Info
+
+https://github.com/Xinyuan-LilyGO/TTGO-T-Display/blob/master/README.MD
+
+https://github.com/Xinyuan-LilyGO/T-Display-S3
---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "esp_err.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "HELLO_WORLD";
+
+void app_main(void)
+{
+  // // Initialize NVS (required for WiFi, but good practice to include)
+  // esp_err_t ret = nvs_flash_init();
+  // if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  // {
+  //   ESP_ERROR_CHECK(nvs_flash_erase());
+  //   ret = nvs_flash_init();
+  // }
+  // ESP_ERROR_CHECK(ret);
+
+  // Keep the app running
+  while (1)
+  {
+    // Print "Hello World" to serial monitor repeatedly
+    ESP_LOGI(TAG, "Hello World!");
+
+    // You can also use printf (requires UART configuration)
+    printf("Hello World from printf!\n");
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
